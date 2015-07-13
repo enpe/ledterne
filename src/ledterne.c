95212ad17e050ee9fa4853bc3e6bffe3f025e761
Alexander Nitsch <nitsch@ht.tu-berlin.de>
2015-07-13 05:38:58 +0200

    Add some documentation

--- src/ledterne.c
+++ src/ledterne.c
@@ -1,5 +1,26 @@
 /**
- * ATmega8 @ 8Mhz
+ * Dim an RGB LED using software PWM
+ *
+ * Platform: ATmega8 @ 8Mhz
+ *
+ * The basic idea is to rapidly switch an LED on and off in order to change its brightness. The LED
+ * is switched on at the beginning of a PWM cycle and switched off later in that cycle (or not
+ * switched off at all if maximum brightness is desired). Depending on how long the LED remains lit
+ * in that cycle, it will appear brighter or darker. The proportion of "on" time to the length of
+ * the complete PWM cycle is the so-called duty cycle.
+ *
+ * Since the LED is basically flickering, the PWM frequency has to be high enough to make the LED
+ * appear to glow with constant brightness to the human eye. We use a PWM frequency of roughly
+ * 100 Hz here, with each PWM cycle consisting of 256 steps (i.e. a temporal resolution of 8 bit).
+ *
+ * A hardware timer is generating an interrupt for each PWM step. The interrupt handler then
+ * decides, based on the current position in the PWM cycle and the desired LED brightness, whether
+ * to switch the LED on or off.
+ *
+ * The human eye does not perceive linear changes in brightness as linear but rather
+ * logarithmically. For a perceived linear change in LED brightness we thus need to map the desired
+ * LED brightness to an appropriate PWM duty cycle by applying a power function. This mapping is
+ * done using a precomputed lookup table.
  */
 
 // clock frequency
@@ -82,9 +103,9 @@ ISR( TIMER1_COMPA_vect )
 	if( pwmStep < g_intensity.r ) { PORTB |= (1<<PIN_R); } else { PORTB &= ~(1<<PIN_R); }
 	if( pwmStep < g_intensity.g ) { PORTB |= (1<<PIN_G); } else { PORTB &= ~(1<<PIN_G); }
 	if( pwmStep < g_intensity.b ) { PORTB |= (1<<PIN_B); } else { PORTB &= ~(1<<PIN_B); }
-	
+
 	// Since it is an uint8, this counter overflows at 255. This is desired behaviour. It makes the
-	// counter run from 0 to 255.
+	// counter run from 0 to 255, i.e. it makes a complete PWM cycle consist of 256 single steps.
 	pwmStep += 1;
 }
 
@@ -102,8 +123,16 @@ void pwmTimerInit()
 	TCCR1B |= (1<<WGM12);
 
 	// frequency of timer interrupts: ca. 25.6 kHz
+	//
 	//     f = F_CPU / (PRESCALER * (1 + OCR1A))
 	// OCRA1 = F_CPU / (f * PRESCALER) - 1
+	//
+	// Since a complete PWM cycle consists of 256 single steps (see the PWM step counter in the
+	// associated interrupt handler) this leaves us with a PWM frequency of roughly
+	//
+	// f_PWM = 25.5 kHz / 256
+	//       = 100 Hz .
+	//
 	OCR1A = 312;
 
 	// enable interrupt on reaching the reference value in OCR1A
@@ -133,6 +162,7 @@ int main( void )
 	sei();
 
 
+	// initial LED intensities
 	uint8_t intensityR = 0;
 	uint8_t intensityG = 21;
 	uint8_t intensityB = 42;
